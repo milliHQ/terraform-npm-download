d2948b50c628a318b1c438ab2a6a39941e416ef9
aluc <b6pzeusbc54tvhw5jgpyw8pwz2x6gs@gmail.com>
2022-01-17 23:34:32 +0900

    chore: add rel_path output for relative path

--- examples/download/main.tf
+++ examples/download/main.tf
@@ -1,11 +1,16 @@
 module "npm_download" {
-  source = "milliHQ/download/npm"
+  # source = "milliHQ/download/npm"
+  source = "../../"
 
   module_name    = "@millihq/tf-next-image-optimization"
   module_version = "11.1.2"
   path_to_file   = "dist.zip"
 }
 
-output "file_path" {
+output "abs_path" {
   value = module.npm_download.abs_path
+}
+
+output "rel_path" {
+  value = module.npm_download.rel_path
 }
